c60d5fb07a1c1c5f346cb8967f3c6979f3469a49
DanielPeregrino18 <dany.peregrinol@gmail.com>
2025-02-22 23:55:18 -0600

    bd

--- ecodrop/settings.py
+++ ecodrop/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'ecodrop.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy',
-        'NAME': 'db'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

343de6171310eb8daaeaadfdf7cdb2155444e23f
DanielPeregrino18 <dany.peregrinol@gmail.com>
2025-03-14 13:53:07 -0600

    Se cambio la base de datos y el objeto que se manda en getEstadisticas

--- administrador/models.py
+++ administrador/models.py
@@ -9,5 +9,15 @@ def verificarAdministrador(id):
     return user.get('admin')
 
 def obtenerEstadisticas():
-    return list(estadisticas.find({}))
+    result = list(estadisticas.find({}))
+    return convert_objectid(result) 
+
+def convert_objectid(data):
+    if isinstance(data, ObjectId):
+        return str(data)
+    elif isinstance(data, dict):
+        return {k: convert_objectid(v) for k, v in data.items()}
+    elif isinstance(data, list):
+        return [convert_objectid(i) for i in data]
+    return data
    
